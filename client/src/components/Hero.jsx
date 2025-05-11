cf7539ed6c20e1a1437dfa4360e4ff76a91ef265
Subhadip006 <subhadip58512@gmail.com>
2025-05-11 11:01:45 -0400

    Button Description Changed

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         <div className='text-2xl font-sans text-[#07474d]'>A public Complaint Management System</div>
         <div className='flex gap-x-4'>
             <Button text='Raise Complaint' color='bg-[#cfb961]' textColor='text-black' hoverColor='bg-[#F7A072]' className='px-6 py-2 rounded font-semibold transition duration-300' />
-            <Button text='Check status' color='bg-[#F7A072]' textColor='text-black' hoverColor='bg-[#F7A072]' className='px-6 py-2 rounded font-semibold transition duration-300' />
+            <Button text='Check Status' color='bg-[#F7A072]' textColor='text-black' hoverColor='bg-[#F7A072]' className='px-6 py-2 rounded font-semibold transition duration-300' />
         </div>
     </div>
     
