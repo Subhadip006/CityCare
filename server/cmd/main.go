218e10a75dc5bd858f3481fc13658b6f84cc2be9
Subhadip006 <subhadip58512@gmail.com>
2025-05-15 06:23:50 -0400

    Specified routes package for simplicity

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/Subhadip006/CityCare/config"
 	"github.com/Subhadip006/CityCare/pkg/db"
-	"github.com/Subhadip006/CityCare/pkg/handlers/auth"
+	"github.com/Subhadip006/CityCare/pkg/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -21,11 +21,10 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "http://localhost:5173",
-		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
 
-	app.Post("/register", auth.Register)
-	app.Post(("/login"), auth.Login)
+	routes.Setup(app)
 
 	app.Listen(":8080")
 
