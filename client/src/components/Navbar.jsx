0006ce793b41a9c85e5f08254e0b5bdf4839fe33
Subhadip006 <subhadip58512@gmail.com>
2025-05-15 10:10:59 -0400

    Modified Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='w-full bg-transparent flex justify-between items-center p-4'>
-        <div className='text-5xl font-semibold pl-10 text-[#0FA3B1]'>Citycare</div>
+        <div className='text-5xl font-semibold pl-10 text-[#0FA3B1]'>
+          Citycare
+          </div>
         <div>
             <ul className='hidden md:flex gap-4 pr-20 text-[#13737c] text-xl font-semibold'>
                 <li>Contacts</li>
