10cefa44608d0df8b5e0f6d8dce42f02950c0b33
Subhadip006 <subhadip58512@gmail.com>
2025-05-28 15:41:16 +0530

    added router for officer login

---
+++ server/pkg/handlers/auth/officelogin.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"github.com/Subhadip006/CityCare/pkg/db"
+	"github.com/Subhadip006/CityCare/pkg/models"
+	"github.com/Subhadip006/CityCare/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func officeLogin(c *fiber.Ctx) error {
+	type req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var body req
+
+	err := c.BodyParser(&body)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid",
+		})
+	}
+
+	var officer models.Officer
+
+	if err := db.DB.Where("email = ?", body.Email).First(&officer).Error; err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "not found",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(officer.Password), []byte(body.Password)); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "password does not match",
+		})
+	}
+
+	token, err := utils.GenerateToken(officer.ID)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "error generating token",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Login Successful",
+		"token":   token,
+	})
+
+}
