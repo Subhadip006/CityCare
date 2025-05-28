759dcc5a5d3c1503f5668a8b8abe4d597726b7d1
Subhadip006 <subhadip58512@gmail.com>
2025-05-28 15:33:42 +0530

    made different login and logout button for users

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import LogoutButton from './LogoutButton';
+import LoginButton from './loginButton';
 
 function Navbar() {
+  const isLoggedIn = false; // Replace this with actual authentication logic
+
   return (
     <div className='w-full bg-transparent flex justify-between items-center p-4'>
         <div className='text-5xl font-semibold pl-10 text-[#0FA3B1]'>
           Citycare
-          </div>
+        </div>
         <div>
             <ul className='hidden md:flex gap-4 pr-20 text-[#13737c] text-xl font-semibold'>
-                <li>Contacts</li>
-                <li>Contacts</li>
-                <li>Contacts</li>
-                <li>Contacts</li>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+                {isLoggedIn ? (
+                  <li>
+                    <LogoutButton />
+                  </li>
+                ) : (
+                  <li>
+                    <LoginButton />
+                  </li>
+                )}
             </ul>
         </div>
     </div>
