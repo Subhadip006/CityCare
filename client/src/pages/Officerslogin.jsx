21153182d54f313b56c5ffd66b68922daae9c6f6
Subhadip006 <subhadip58512@gmail.com>
2025-05-29 22:28:34 +0530

    not an important commit (ignore)

--- client/src/pages/Officerslogin.jsx
+++ client/src/pages/Officerslogin.jsx
@@ -8,14 +8,17 @@ function Officerslogin() {
   const [success, setsuccess] = useState('');
   const handleSubmit = (e) =>{
     e.preventDefault();
-    console.log("hello");
+    
+    setsuccess('');
+    seterror('');
+
   } 
 
 
   return (
     <div className='min-h-screen flex items-center justify-center bg-[#F9F7F3] text-[#cfb961] font-bold font-sans'>
       <div className='w-[90%] md:w-[50%] lg:w-[30%] bg-white p-8 rounded-lg shadow-lg border border-[#cfb961]'>
-        <h1 className='text-3xl text-center font-extrabold mb-4 '>Login</h1>
+        <h1 className='text-3xl text-center font-extrabold mb-4 '>Office Login</h1>
 
         {error && <div className="text-red-500 mb-4 text-center">{error}</div>}
         {success && <div className="text-green-600 mb-4 text-center">{success}</div>}
@@ -56,7 +59,7 @@ function Officerslogin() {
         <div className='mt-4 text-center'>
           <p className='text-sm'>
             Don't have an account?
-            <Link to={'/register'} className='text-[#F7A072] pl-1 hover:underline'>
+            <Link to={'/officerRegister'} className='text-[#F7A072] pl-1 hover:underline'>
               Register
             </Link>
           </p>
