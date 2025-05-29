deabe1af9470c89918b778f2d9356d289ad0371f
Subhadip006 <subhadip58512@gmail.com>
2025-05-29 22:30:16 +0530

    made minor changes on officers model

--- server/pkg/models/officers.go
+++ server/pkg/models/officers.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type Officer struct {
 	gorm.Model
-	Username string `gorm:"not null"`
-	Email    string `gorm:"unique;not null"`
-	Password string `gorm:"not null" json:"password,omitempty"`
+	Username   string `gorm:"not null"`
+	Email      string `gorm:"unique;not null"`
+	Password   string `gorm:"not null" json:"password,omitempty"`
+	Department string `gorm:"not null"`
 }
