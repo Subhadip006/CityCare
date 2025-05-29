34afd3bd626d059fb602fce7516454a19cf0a79c
Subhadip006 <subhadip58512@gmail.com>
2025-05-29 22:26:06 +0530

    made changes for log out button

---
+++ client/src/components/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LogoutButton() {
+  return (
+            <button className='bg-[#f01e2c] py-1 px-4 cursor-pointer rounded-3xl text-gray-100 mb-2'>
+                Log Out
+            </button>
+  )
+}
+
+export default LogoutButton
