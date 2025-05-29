a1c562018d5a8fb87f71a42d642e81025658fd19
Subhadip006 <subhadip58512@gmail.com>
2025-05-29 22:25:37 +0530

    made changes in login button

--- client/src/components/loginButton.jsx
+++ client/src/components/loginButton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function LoginButton() {
     return (
         <Link to = '/login'>
-            <button className='bg-[#0FA3B1] py-1 px-2 cursor-pointer rounded-3xl text-gray-100 mb-2'>
+            <button className='bg-[#0FA3B1] py-1 px-4 cursor-pointer rounded-3xl text-gray-100 mb-2'>
             Login/Signup
             </button>
         </Link>
