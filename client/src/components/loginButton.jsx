bbc6f5aaa1153b0858b7b0b33ec7d8cb267add6c
Subhadip006 <subhadip58512@gmail.com>
2025-06-01 21:02:34 +0530

    changed the button only for login

--- client/src/components/loginButton.jsx
+++ client/src/components/loginButton.jsx
@@ -5,7 +5,7 @@ function LoginButton() {
     return (
         <Link to = '/login'>
             <button className='bg-[#0FA3B1] py-1 px-4 cursor-pointer rounded-3xl text-gray-100 mb-2'>
-            Login/Signup
+               Login
             </button>
         </Link>
 
