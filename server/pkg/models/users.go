cdbd8c7de8f1ff2b98fa718b66cd18c34ddc1912
Subhadip006 <subhadip58512@gmail.com>
2025-06-01 21:21:31 +0530

    changed role property for users models

--- server/pkg/models/users.go
+++ server/pkg/models/users.go
@@ -4,18 +4,10 @@ import (
 	"gorm.io/gorm"
 )
 
-type Role string
-
-const (
-	CitizenRole Role = "citizen"
-	OfficerRole Role = "officer"
-	AdminRole   Role = "admin"
-)
-
 type User struct {
 	gorm.Model
 	Username string `gorm:"not null"`
 	Email    string `gorm:"unique;not null"`
 	Password string `gorm:"not null" json:"password,omitempty"`
-	Role     Role   `gorm:"type:varchar(20);not null"`
+	Role     string `gorm:"type:varchar(20);not null"`
 }
